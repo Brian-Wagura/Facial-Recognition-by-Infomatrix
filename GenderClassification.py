f264a2a059a51577c587a807524fb3afc1f5519c
wagura-droid <briankiraguri@gmail.com>
2022-11-13 13:21:36 +0300

    Gender Classification Code Update

---
+++ GenderClassification.py
@@ -0,0 +1,155 @@
+# Import Dependencies
+import cv2
+import os
+import numpy as np
+import warnings
+from keras.utils import np_utils
+from keras.models import Sequential, load_model
+from keras.layers import Dense,Activation,Flatten,Dropout
+from keras.layers import Conv2D, MaxPooling2D
+from keras.callbacks import ModelCheckpoint
+from sklearn.model_selection import train_test_split
+
+data_path   = 'datasets'
+# Categories in the folder
+categories  = os.listdir(data_path)
+labels      = [ i for i in range(len(categories))]
+# zip function helps to create the dictionary
+label_dict  = dict(zip(categories,labels))
+
+# print(label_dict) # {'female': 0, 'male': 1}
+# print(categories) # ['female', 'male']
+# print(labels)     # [0, 1]
+
+img_size = 32
+data     = []
+target   = []
+
+# Detects face
+cascade  = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+
+for category in categories:
+    folder_path   = os.path.join(data_path,category)
+    img_names     = os.listdir(folder_path)
+
+    for img_name in img_names:
+        img_path    = os.path.join(folder_path,img_name)
+        img         = cv2.imread(img_path)
+        faces       = cascade.detectMultiScale(img)
+
+        try:
+            for f in faces:
+                # Detect co-ordinates
+                x,y,w,h   = [v for v in f]
+                # Cut the co-ordinate of the picture
+                sub_face  = img[y:y+h, x:x+w]
+                # Convert the picture into gray scale
+                gray      = cv2.cvtColor(sub_face, cv2.COLOR_BGR2GRAY)
+                # Cut down the image into 32bytes x 32bytes
+                resized   = cv2.resize(gray, (img_size,img_size))
+                # Append to data list
+                data.append(resized)
+                # Append to target list
+                target.append(label_dict[category])
+        except Exception as e:
+            print('Exception:',e)
+
+warnings.filterwarnings('ignore')
+# Convert into the range 0 and 1
+data  = np.array(data)/255.0
+data  = np.reshape(data, (data.shape[0],img_size,img_size,1))
+target= np.array(target)
+
+new_target  = np_utils.to_categorical(target)
+
+# Saves into training folder
+np.save('./training/data', data)
+np.save('./training/target', new_target)
+
+data   = np.load('./training/data.npy')
+target = np.load('./training/target.npy')
+
+# Create the Model
+noOfFilters    = 64
+sizeOfFilter1  = (3,3)
+sizeOfFilter2  = (3,3)
+sizeOfPool     = (2,2)
+noOfNode       = 64
+
+model = Sequential()
+
+model.add((Conv2D(32, sizeOfFilter1, input_shape=data.shape[1:], activation='relu')))
+model.add((Conv2D(32, sizeOfFilter1, activation='relu'))) 
+model.add(MaxPooling2D(pool_size=sizeOfPool))
+
+model.add((Conv2D(64, sizeOfFilter2, activation='relu')))
+model.add((Conv2D(64, sizeOfFilter2, activation='relu')))
+model.add(MaxPooling2D(pool_size=sizeOfPool))
+model.add(Dropout(0.5))
+
+model.add(Flatten()) #Linear Format
+model.add(Dense(noOfNode,activation='relu'))
+model.add(Dropout(0.5))
+# Number of Classes(male,female) softmax-helps in binary classification
+model.add(Dense(2, activation='softmax')) 
+
+#Compile the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Split data into train and test data
+train_data, test_data, train_target, test_target = train_test_split(data,target,test_size=0.1)
+
+# Initialize checkpoint(saving each model  in training folder)
+checkpoint  = ModelCheckpoint('./training/model-{epoch:03d}.model', monitor='val_loss', verbose=0, save_best_only=True, mode='auto')
+history     = model.fit(train_data, train_target, epochs=30, callbacks=[checkpoint], validation_split=0.2)
+
+# Load a model from the training folder
+model       = load_model('./training/model-022.model')
+# Detects face
+face_clsfr  = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+# opencv video capture
+cap         = cv2.VideoCapture(0)
+
+label_dict  = {0:'Female', 1:'Male'}
+color_dict  = {0:(0,0,255),1:(0,255,0)}
+
+while True:
+
+    ret,img = cap.read()
+    gray    = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces   = face_clsfr.detectMultiScale(gray,1.3,3)
+
+    for (x,y,w,h) in faces:
+        face_img   = gray[y:y+w, x:x+w]
+        resized    = cv2.resize(face_img, (32,32))
+        normalized = resized/255.0
+        reshaped   = np.reshape(normalized, (1,32,32,1))
+
+        # Predict the result
+        result     = model.predict(reshaped)
+        # Helps to give output
+        label      = np.argmax(result,axis=1)[0]
+
+        # Rectangle of our faces
+        cv2.rectangle(img, (x,y), (x+w, y+h), color_dict[label],2)
+        # Rectangle of label
+        cv2.rectangle(img, (x,y-40), (x+w,y), color_dict[label], -1)
+        cv2.putText(img, label_dict[label], (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,255), 2)
+
+    cv2.imshow('Result', img)
+    k  = cv2.waitKey(1)
+
+    if k == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+cap.release()
+
+
+
+ 
+
+
+
+            
--- README.md
+++ README.md
@@ -1,15 +1,35 @@
 # Facial-Recognition-by-Infomatrix
 This is a 4th year group project by Group F submitted to Mr Mokodir, Decision Support Systems Lecturer.
 
-Title: Face recognition program
+# Title: Face Recognition Program
 
-Objective: to build a face recognition program that does not gender assasinate but detects the gender of a person just by scanning their face
+# Objective: To build a face recognition program that does not gender assasinate but detects the gender of a person just by scanning their face
 
-About the project
+# Dataset
+This are the female and male subfolders in the datasets directory containing more than 100 pictures of female and male face pictures in each 
+respective subdirectory.
+  
+# About the project
+This project classifies faces according to their gender; either Female or Male.
+Convolutional Neural Networks are used to build the model.
+It uses opencv-python to video capture and the gender is predicted by the model.
+A squarebox is displayed predicting if either the person is female or male.
+By showing the camera a picture, the program will also predict if either the picture displayed is male or female
 
-Dataset
-
-How you test
-
-visualization
+# How you test
+-Clone the repository and change directory into the folder:
+ git clone https://github.com/wagura-droid/Facial-Recognition-by-Infomatrix.git
+-Install virtual environment:
+ sudo apt install python3.10-venv
+-Create virtual environment
+ python3 -m venv <virtualenvname>
+-Activate virtual environment:
+ source <virtualenvname>/bin/activate
+-Install Dependencies
+ pip install -r requirements.txt
+-Run the program
+ python3 GenderClassification.py
+  
+# Visualization
+Inside the screenshots folder.
 
